e4aa7fb1bddc4952528e935f8904e66e28b3adee
k0rteksz <daniel.kiraly@outlook.com>
2020-06-17 17:47:40 +0200

    fdséhfaksbk

--- js/price.js
+++ js/price.js
@@ -1,8 +1,8 @@
 function calcPrice() {
     
-    let count = parseInt(document.querySelector("#count").value, 10);
-    let sauce = parseInt(document.querySelector("#sauce").value, 10);
-    let extra = parseInt(document.querySelector("[name-extra]:checked").value, 10);
+    let count = parseInt(document.querySelector("input[name='count']"));
+    let sauce = parseInt(document.querySelector("input[name='sauce']"));
+    let extra = parseInt(document.querySelector("[name-extra]:checked"));
     let price = parseInt(1200 + sauce + extra) * count;
     let cost = document.querySelector("span.show-price");
     
