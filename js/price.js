024f9b6c1941dcea18559e97a902e14be951892a
k0rteksz <daniel.kiraly@outlook.com>
2020-06-18 23:28:35 +0200

    a

--- js/price.js
+++ js/price.js
@@ -1,10 +1,8 @@
-function calcPrice() {
-    
-    let count = parseInt(document.querySelector("input[name='count']"));
-    let sauce = parseInt(document.querySelector("input[name='sauce']"));
-    let extra = parseInt(document.querySelector("[name-extra]:checked"));
-    let price = parseInt(1200 + sauce + extra) * count;
-    let cost = document.querySelector("span.show-price");
-    
-    cost.innerHTML=price;
+function sendOrderData() {
+    const message = document.querySelector('#message');
+    const extra = parseInt(document.querySelector('[name=extra]:checked').value, 10);
+    const sauce = parseInt(document.querySelector('#sauce').value, 10);
+    const quantity = parseInt(document.querySelector('#quantity').value, 10);
+    const price = (1200 + extra + sauce) * quantity;
+    message.innerText = price;
 }
